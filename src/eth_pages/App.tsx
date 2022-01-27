303e61e6d68958c67a91fbdcfc2d27c66ac3ce75
Demari C <demaricameron@outlook.com>
2022-01-27 13:53:59 -0500

    fix: ui updates, connect wallet

--- src/eth_pages/App.tsx
+++ src/eth_pages/App.tsx
@@ -1,6 +1,9 @@
 import { ChainId } from '@sushiswap/sdk'
 import React, { Suspense, useEffect, useRef } from 'react'
 import { Redirect, Route, Switch, useLocation } from 'react-router-dom'
+import TelegramIcon from '@mui/icons-material/Telegram'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import GitHubIcon from '@mui/icons-material/GitHub'
 // import Header from '../eth_components/Header'
 import Polling from '../eth_components/Header/Polling'
 import Popups from '../eth_components/Popups'
@@ -9,6 +12,8 @@ import ClaimSokuModal from '../eth_components/ClaimSokuModal'
 import PublicRoute from '../hocs/PublicRoute'
 import SokuMenu from '../eth_components/SokuMenu'
 import SlideOutMenu from '../eth_components/SlideOutMenu/SlideOutMenu'
+import AccountModal from '../eth_components/AccountModal'
+
 import { useActiveWeb3React } from '../eth_hooks/index'
 import { useWalletModalToggle } from '../eth_state/application/hooks'
 import Connect from '../kashi/pages/Connect'
@@ -104,6 +109,16 @@ function App(): JSX.Element {
     const truncatedFirstHalf = account?.substring(0, 5)
     const truncatedLastHalf = account?.substring(account.length - 5, account.length)
     const truncatedAddress = `${truncatedFirstHalf}...${truncatedLastHalf}`
+
+    const openHiddenLinks = () => {
+        const hiddenLinks = document.getElementsByClassName('hidden_navLinksMobile')
+        // console.log(hiddenLinks)
+        if (hiddenLinks[0]?.id === 'hidden_navLinks') {
+            hiddenLinks[0].id = 'open'
+        } else if (hiddenLinks[0]?.id === 'open') {
+            hiddenLinks[0].id = 'hidden_navLinks'
+        }
+    }
 
     const isMobile = window.innerWidth <= 500
 
@@ -213,7 +228,9 @@ function App(): JSX.Element {
                 <div className="connectWallet__options__MOBILE">
                     <ul>
                         {account ? (
-                            <li className="account__footer">Account: {truncatedAddress}</li>
+                            <li className="account__footer">
+                                <AccountModal />
+                            </li>
                         ) : (
                             <li className="connectWallet" onClick={toggleWalletModal}>
                                 <button type="button">Connect Wallet</button>
@@ -227,6 +244,7 @@ function App(): JSX.Element {
                                 type="submit"
                                 className="material-icons"
                                 style={{ backgroundColor: '#ebebeb', color: '#05195a', padding: '2px 5px' }}
+                                onClick={openHiddenLinks}
                             >
                                 more_horiz
                             </button>
@@ -234,21 +252,60 @@ function App(): JSX.Element {
                     </ul>
                     <ul className="hidden_navLinksMobile" id="hidden_navLinks">
                         <li>
-                            <a href="https://www.sokuswap.finance/" rel="noreferrer noopener" target="_blank">
-                                <span className="material-icons">info</span>
-                                <p>About</p>
+                            <a href="/" rel="noreferrer noopener" target="_blank">
+                                <span className="material-icons">analytics</span>
+                                <p>Analytics</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://sokuswap-2.gitbook.io/sokuswap-gitbook/"
+                                rel="noreferrer noopener"
+                                target="_blank"
+                            >
+                                <span className="material-icons">school</span>
+                                <p>Docs</p>
                             </a>
                         </li>
                         <li>
                             <a href="https://github.com/Soku-Swap-Project" rel="noreferrer noopener" target="_blank">
-                                <span className="material-icons">code</span>
-                                <p>Code</p>
+                                <GitHubIcon />
+                                <p>GitHub</p>
+                            </a>
+                        </li>
+                        <div
+                            className="social_icon_headerMobile"
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                paddingTop: '18px'
+                            }}
+                        >
+                            <p style={{ fontSize: '14px', marginLeft: '-5px' }}>Social Links</p>
+                        </div>
+                        <div className="social_icon_row" style={{ display: 'flex', justifyContent: 'center' }}>
+                            <hr
+                                style={{
+                                    width: '65%',
+                                    marginTop: '10px',
+                                    paddingTop: '0',
+                                    borderColor: '#05195a'
+                                }}
+                                className="disabled_link"
+                            />
+                        </div>
+
+                        <li>
+                            <a href="https://t.me/SokuSwap" rel="noreferrer noopener" target="_blank">
+                                <TelegramIcon />
+                                <p>Telegram</p>
                             </a>
                         </li>
                         <li>
-                            <a href="/" rel="noreferrer noopener" target="_blank">
-                                <span className="material-icons">analytics</span>
-                                <p>Analytics</p>
+                            <a href="https://twitter.com/sokuswap" rel="noreferrer noopener" target="_blank">
+                                <TwitterIcon />
+                                <p>Twitter</p>
                             </a>
                         </li>
                     </ul>
