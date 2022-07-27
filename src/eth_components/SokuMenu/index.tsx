74554d89cdf1b697e633b1f2351be2955593812e
ChefMari <demaricameron@outlook.com>
2022-07-27 01:02:14 -0400

    feat: update swap

--- src/eth_components/SokuMenu/index.tsx
+++ src/eth_components/SokuMenu/index.tsx
@@ -63,7 +63,10 @@ const SokuMenu: React.FC = props => {
                                 <div className="sokuswap__navbar">
                                     <nav className="soku_nav">
                                         <ul className="navbar__items">
-                                            <a className="nav_link hover_transparent" href={`${origin}/swap`}>
+                                            <a
+                                                className="nav_link hover_transparent"
+                                                href="https://swap.app.sokuswap.finance"
+                                            >
                                                 Trade
                                             </a>
                                             <NavLink
